5168ef5cab358dedc8ec48f27c82944ac0e9d44a
Ryhlinskaya <Ryhlinskaya@bk.ru>
2021-12-16 18:41:42 +0700

    Create and write interface GameObjectMovement

---
+++ SokoBan/GameObjectMovement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SokoBan
+{
+    interface GameObjectMovement
+    {
+        public void MoveTo(string direction);
+        public bool IsTakenHere(int x, int y);
+    }
+}
