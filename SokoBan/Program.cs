51523546fa4fa35c037a30b3d39dc363a368d6db
Ryhlinskaya <Ryhlinskaya@bk.ru>
2021-12-16 18:30:26 +0700

    Добавьте файлы проекта.

---
+++ SokoBan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SokoBan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
